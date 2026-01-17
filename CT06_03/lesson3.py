0e1f773cd292c009a91ce67850c4327306eb4e04
Computhink-CS137 <cs137@computhink.com.sg>
2026-01-17 07:31:47 +0000

    lesson 2

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,3 +1,4 @@
+import math
 # number1 = 100
 # print(number1)
 
@@ -16,7 +17,11 @@
 # print(variable1 * variable2)
 # print(variable1 / variable2)
 # print(2 + 4 * 5 / 6 + 3 - 6 / 2)
-# Task 3
+#Task 3(rounding)
+# number = 42.56789
+# number = round(number)
+# print(number)
+# Task 4
 # 3a
 # a = 10
 # b = 15
@@ -31,3 +36,46 @@
 # 3d
 # ans4 = (3 + 4) * 5 - (10 / 2)
 # print(ans4)
+# task 5 a
+# pineapple_tarts = 15 * 2
+# bak_kwa = 58
+# total = pineapple_tarts + bak_kwa
+# print(total)
+# task 5 b
+# seafood = 96
+# side_dish = 18 * 3
+# rice = 2 * 6
+# total = (seafood + side_dish + rice) * 1.1 - 25
+# print(round(total, 2))
+# task 5c
+# seafood = 100 * 5
+# side_dish = 20 * 8
+# rice = 2.5 * 20
+# total = (seafood + side_dish + rice) * 1.1 
+# print(round(total, 2))  
+# 6a
+# movement = -55
+# distance = abs(movement)
+# print(distance)
+# 6b
+# change = -8
+# real_change = abs(change)
+# print(real_change)
+# task 7a
+# ingredients = 9.8
+# ing_per_batch = 1
+# total = math.floor(ingredients) / ing_per_batch
+# print(total)
+# oranges = 27
+# per_bag = 4
+# total = oranges / per_bag
+# ans = math.ceil(total)
+# print(ans)
+# task 8a 
+# diameter = 100
+# distance = diameter * math.pi
+# print(round(distance, 2))
+# task 8b
+radi_pizza = 12
+area_pizza = radi_pizza ** 2 * math.pi
+print(round(area_pizza, 2))
--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,12 @@
-print("Hello from lesson 4")
+#print("Hello from lesson 4")
+# red = 1 * 3
+# blue = 2 * 5
+# green = 3 * 4
+# ans = red + green + blue 
+# print(ans)
+# name = "samaira"
+# print("my name is " + name)
+myName = "apple"
+print(myName)
+myName = input("whats your name? ")
+print(myName)

d4c865ff9c181bccbca25cef513c4e892e506643
Computhink-CS137 <cs137@computhink.com.sg>
2026-01-24 07:36:10 +0000

    lesson 3

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -6,7 +6,87 @@
 # print(ans)
 # name = "samaira"
 # print("my name is " + name)
-myName = "apple"
-print(myName)
-myName = input("whats your name? ")
-print(myName)
+# myName = "apple"
+# print(myName)
+# task 1a
+# myName = input("whats your name? ")
+# print("your name is " + myName)
+# task 1b
+# fav_color = input("what is you favorite color? ")
+# print("your favorite color is " + fav_color)
+# task 1c
+# age = input("how old are you? ")
+# print( "you are " + age + " years old" )
+#  task 2 
+# name = input("what is your name? " )
+# print("hi, " + name + " it's nice to meet you")
+# hobby = input("what is your hobby? ")
+# print( hobby + ", that sounds fun!!!")
+# vacay = input( "what is your dream vacation place? ")
+# print("I would love to visit " + vacay)
+# print (vacay * 6 )
+# number = 100
+# print("number: " + str(number)) # TypeCasting
+# number = "100"
+# print( int(number) + 10 )
+# task 3a
+# age = int(input("how old are you? "))
+# age = age + 10
+# age = str(age)
+# print( "you are " + age + " years old, in 10 years time" )
+# task 3b
+# number = input("can you tell me any number? ")
+# number = int(number)
+# number = number * 2
+# number = str(number)
+# print("double of your number is " + number )
+#  task 3 c
+# year = input("what year were you born in? ")
+# year = int(year)
+# current_year= 2026
+# age = current_year - year
+# age = str(age) 
+# print(" this year you will be " + age + " years old")
+# task 3d
+# number = input(" can you give me a number? ")
+# number = int(number)
+# word = input(" can you give me a word? ")
+# print(word * number)
+# task 4 reassingment
+# message = "hello"
+# message = 10
+# print(message)
+#  task 5 printing full name 
+# first_name = input( " what is your first name? ")
+# last_name = input (" what is your last name? ")
+# print(" your full name is " + first_name +" " + last_name )
+# name = input(" what is your name? ")
+# age = input(" how old are you? ")
+# print( name + " is " + age + " years old")
+#  task 7
+# length = input("how wide is your rectangle? ")
+# length = int(length)
+# width = input("how long is your rectangle? ") 
+# width = int(width)
+# area = length * width
+# area = str(area)
+# print( " the area of your rectangle is "  + area)
+# task 8
+# minutes = input("can you tell me an amount of minutes? " )
+# seconds = input("can you tell me an amount of seconds")
+# seconds = int(seconds)
+# minutes = int(minutes)
+# minutes = minutes * 60
+# total_time = seconds + minutes 
+# total_time = str(total_time)
+# print("you total time is " + total_time + " seconds")
+
+
+
+
+
+
+
+
+
+

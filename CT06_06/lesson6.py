d8622fb158daa69b1e4ba9e4ed0b99450ca508fd
Computhink-CS137 <cs137@computhink.com.sg>
2026-02-07 07:32:56 +0000

    lesson 5

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,60 @@
-print("Hello from lesson 6")
+# # errors and debugging
+# # name error = a typo in your words or 
+# # type error = you can't add an int and a str
+# # syntax error = missing punctuation
+# print("Hello from lesson 6")
+# # task 1 a print("Hello, World!")
+# print("Hello, World!")
+# # task 1 b print(i)
+# print("i")
+# # task 1 c print("Hello, World!)
+# print("Hello, World!")
+# # task 1 d if = 5
+# If = 5
+# print(If)
+# name erorrs
+# # task 2a print(age)
+# age = 12
+# print(age)
+# # task 2b
+# # name = "Alice"
+# # print(name)
+# # task 2 c
+# # print(x)
+# # x = 5
+# x = 5
+# print(x)
+# #  task 2d
+# # pint("hello world")
+# print("hello world!")
+# # *Task 3a**:
+# # age = "25"
+# # print(age + 1)
+# age = 25
+# print(age + 1)
+# # **Task 3b**:
+# # number = 10
+# # print(number - "5")
+# number = 10
+# print(number - 5)
+
+# # **Task 3c**:
+# # print("Repeat" * "3")
+# print("Repeat" * 3)
+
+
+# # **Task 3d**:
+# # year = 2023
+# # print("The year is " + year)
+# year = "2023"
+
+# # **Task 3e**:
+# # x = "10"
+# # y = x / 2
+# x = 10
+
+# # **Task 3f**:
+# # end = "5"
+# # for i in range(end):
+# #     print(i)
+# end = 5
--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,55 @@
 print("Hello from lesson 7")
+# for loops
+# for i in range(start, stop, step)
+#   print("a")
+#   print("b")
+# if start is 1 and stop is 10
+# then i will go from 1, 2, 3, 4, 5,6, 7, 8, 9, 10
+
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# # average_score = total / 3
+
+# # student_name = "Alex"
+
+
+# # print("Average score for " + student_name + " is: " + str(average_score))
+# # You have been tasked to create a programme that calculates the
+# average marks of a class.
+
+# Your programme will have to ask the
+# user for the total number of students, followed by the marks of
+# each student one at a time.
+
+# Use only variables, math operators that you have learnt, as
+# well as a 'for' loop.
+# total_marks = 0
+# student_numbers = int(input("how many student are there?\n"))
+# for i in range(student_numbers):
+#******* F strings
+#*******marks = int(input(f"enter student (i + 1)'s mark:\n"))
+#     marks = int(input("enter marks:\n"))
+#     total_marks = total_marks + marks
+# # you are adding "marks" in to "total marks" so each time it changes it dosen't mater as it will allready be saved in marks  
+#     average = total_marks/ student_numbers
+#     print("the average is " + str(average))
+
+# f-strings = formatted string
+# jhon = 29
+# print(f"Total: (jhon)")
+# recap 3 f strings
+name = input("what is your name?")
+age = input("how old are you?") 
+# non f string
+# print("hello" + name + ", you are" + age + "years old!")
+# f string way
+print(f"Hello, {name}), you are, {age}, years old!")
+#  in f strings you dont need plus signes you just use commas to space them out, 
+# and you use the curvy brackets next to the letter "p" to hold you variable
+
 

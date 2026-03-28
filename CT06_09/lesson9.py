9b2d5d509f26480d1d740ac3ad600d8ee9c3fba9
Computhink-CS137 <cs137@computhink.com.sg>
2026-03-28 07:36:08 +0000

    lesson9

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,60 @@
-print("Hello from lesson 9")
+# lesson 9
+# lesson 8 recap
+# import random
+
+# num1 = random.randint(1, 6)
+# num2 = random.randint(1, 6)
+# num3 = random.randint(1, 6) 
+
+# print(f"first die:  {num1}")
+# print(f"first die:  {num2}")
+# print(f"first die:  {num3}")
+
+# odd = 0
+# even = 0
+
+# if num1 % 2 == 1:
+#     odd = odd + 1
+# else:
+#     even = even + 1
+
+# if num2 % 2 == 1:
+#     odd = odd + 1
+# else:
+#     even = even + 1
+
+# if num3 % 2 == 1:
+#     odd = odd + 1
+# else:
+#     even = even + 1
+
+
+# if odd == 3:
+#     odd = True
+# else:
+#     odd = False
+
+# if even == 3:
+#     even = True
+# else:
+#     even = False 
+
+# if even == True or odd == True:
+#     print(f"All numbers are even or odd.")
+# else:
+#     print("not all the numbers are fully even or odd.")
+
+# # task 2(library books)
+# num_days = int(input("how many days have you borrowed your book for?\n"))
+# if num_days > 25:
+#     print("you need to return your book soon!")
+
+# task 3 (apples)
+# num_apples = int(input(" how many apples do you wan't to buy?\n"))
+# cost_apple = 0
+# if num_apples > 10:
+#     cost_apple = num_apples * 0.9
+# else:
+#     cost_apple = num_apples * 1
+# cost_apple = str(round(cost_apple, 2))
+# print(f"the total cost of apples: ${cost_apple}")

d8bad42b46f6b776ea0649b3f456df6cae4404f8
Computhink-CS137 <cs137@computhink.com.sg>
2026-04-04 07:34:02 +0000

    lesson 10

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,60 @@
-print("Hello from lesson 10")
+#print("lesson 10")
+# task 1
+#num = int(input("can you enter a number?\n"))
+#if num > 0:
+#    print(f" {num} is a positive number")
+#else:
+#   print(f"{num} is a negative number")
+# task 2 (nested if function)
+#age = int(input("how old are you?\n"))
+#if age < 13:
+#    print("you are a child")
+#else:
+#   if age < 19:
+#       print(" you are a teen")
+#    else:
+#       print("you are an adult")
+# elif
+#temp = int(input(" what is the temperature outside in degrees celcuis?\n"))
+#if temp >= 30:
+#    print(" you should go swimming")
+#elif temp > 24:
+#    print(" play basketball")
+#elif temp >= 20:
+#    print(" go cycling")
+#else:
+#    print("read at home")
+# grading system elif
+#grade = int(input(" what grade did you get out of 100?\n"))
+#if grade >= 90:
+#    print(" you got an A")
+#elif grade >= 80:
+#    print(" you got a B")
+#elif grade >= 70:
+#    print(" you go a C")
+#elif grade >= 60:
+#    (" you got a D")
+#else:
+#   print(" sorry, you got F")
+
+#age = int(input(" how old are you?"))
+#if age < 0:
+#    print("invalid age")
+#elif age > 100:
+#    print(" invalid age")
+#elif age >= 18:
+#    print(" you are allowed to vote")
+#else:
+#    print(" sorry, you can't vote")
+
+#money = int(input(" how much money do you have?"))
+#if money > 150:
+#    print(" you can buy a gaming console")
+#elif money > 100:
+#    print(" you can buy a new game")
+#elif money > 50:
+#    print(" you can buy a new mouse")
+#elif money > 20:
+#    print(" you can buy a new laptop cover")
+#else:
+#    print("you can buy snacks")
--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,57 @@
-print("Hello from lesson 11")
+##print("Hello from lesson 11")
+# ********* task 1 ***********
+#rider1 = 120
+#rider2 = 150
+#if rider1 >= 120 and rider1 >= 120:
+#    print(" you both can go on the ride!")
+#else:
+#    print(" you both can't go on the ride")
+#******* task 2 *********
+#num = int(input(" can you enter a number?\n"))
+#if num % 3 == 0 and num % 7 == 0:
+#    print(f"{num} is divisable by 3 and 7")
+#else:
+#    print(f"{num} isn't divisable by 3 and 7")
+#******** task 3 *********
+#name1 = input("what is your first name?").lower()
+#name2 = input(" what is you last name?").lower()
+#if name1 == "james" and name2 == "leong":
+#    print(" you are wanted!!!!")
+#else:
+#    print(" your safe")
+# ******** task 4 **********
+#rider1 = int(input(" how old are you?"))
+#rider2 = int(input(" how old is your partner"))
+#if rider1 >= 18 or rider2 >= 18:
+#    print(" you can go on this ride")
+#else:
+#    print(" sorry you're to young")
+# ********** task 5 ************
+#age = int(input("how old are you?"))
+#if age < 12 or age > 64:
+#    print(" your ticket costs $15")
+#else:
+#    print(" your ticket costs $20")
+# *********** task 6 ***********
+#gender = input("what is your gender").lower()
+#if gender == "female" or gender == "male":
+#    print(" valid gender")
+#else:
+#    print(" invalid response")
+# ********* task 9 ********
+#ans = input("what is the password?")
+#if not ans == "python123":
+#    print("acsess denied")
+#else:
+#    print("acsess granted")
+#********* task 11 **********
+username = "Jhon123"
+password = "pw123"
+user_input = input(" what is your username?")
+pass_input = input("what is your password?")
+if user_input == username and pass_input == password:
+    print(" acsess granted")
+elif user_input == username or pass_input == password:
+    print(" either username or password is incorect")
+else:
+    print(" accses denied")

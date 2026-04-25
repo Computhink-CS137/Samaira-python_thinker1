8c75ae32011fe79cdcf1a0a938d5236b2cfb9588
Computhink-CS137 <cs137@computhink.com.sg>
2026-04-25 07:22:41 +0000

    lesson 13
    Co-authored-by: trainer1-computhink <trainer1-computhink@users.noreply.github.com>

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,50 @@
-print("Hello from lesson 13")
+#Catolouge = []
+#while True:
+#    user_input = input("what do you want to buy?")
+#    if user_input == "end":
+#        break
+#    Catolouge.append(user_input)
+#print(Catolouge)
+
+#ask = input("what are you looking for?")
+#if ask in Catolouge:
+#    print(f" yes we have {ask}")
+#else:
+#    print(f"we don't have {ask}")
+
+# TAsk 5
+#import random
+#box = []
+#for i in range(10):
+#    num = random.randint(1, 999)
+#    box.append(num)
+#print(box)
+
+#for i in range(len(box)):
+#    print(f"Winner #{i + 1} = {box[i]}")
+
+#Task 6
+toppings = []
+toppings.append("sauce")
+toppings.append("Extra Cheese")
+toppings.append("basil")
+toppings.append("cucumber")
+toppings.append("mushrooms")
+toppings.append("carrots")
+toppings.append("tomato")
+toppings.append("corn")
+toppings.append("olives")
+toppings.append("garlic")
+for i in range(len(toppings)):
+    print(f" # { i + 1} = {toppings [i]}")
+order_list = []
+while True:
+    ans = input(" CHoose your topping by number?")
+    if ans == "end":
+        break
+    order_list.append(int(ans))
+
+
+for i in range(len(order_list)):
+    print(toppings[order_list[i] - 1])
+
